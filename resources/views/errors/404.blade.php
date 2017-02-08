8b85a82668cafdfd875fd7351e4f94c672fd3424
Rassel Jan David <rasseljandavid@Rassels-MacBook-Pro.local>
2017-02-08 16:44:28 +0800

    Add 404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,1 +1,16 @@
-Not found
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+      <div class="row">
+        <!--Middle Part Start-->
+        <div id="content" class="col-sm-12">
+          <h1 class="title-404 text-center">404</h1>
+          <p class="text-center lead">Sorry Folks!<br>
+            The page you requested cannot be found! </p>
+          <div class="buttons text-center"> <a class="btn btn-primary btn-lg btn-red" href="/">Continue</a> </div>
+        </div>
+        <!--Middle Part End -->
+      </div>
+    </div>
+@endsection
